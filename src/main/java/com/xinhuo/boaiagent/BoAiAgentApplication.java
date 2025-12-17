354a75aa7275c51ff2fc8e7177e9e619a8d929c9
Heart-fire <wangchunbo384@gmail.com>
2025-12-17 22:36:24 +0800

    初始化

---
+++ src/main/java/com/xinhuo/boaiagent/BoAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xinhuo.boaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinhuo/boaiagent/BoAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinhuo.boaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
